1cc023d7fa4807c91cdb6e969d4b7c543e59a35e
Collins Orighose <collinsefe@yahoo.com>
2025-02-27 17:38:36 +0000

    refactored code

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "mp-kubernetes-tfstate"
-    key    = "infra/infra.tfstate"
-    region = "eu-west-2"
-  }
-}
-
-
--- terraform/ebs.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_ebs_volume" "redis_volume" {
-  availability_zone = "eu-west-2a" # Change to your desired AZ
-  size              = 10           # Size of the EBS volume (in GiB)
-  type              = "gp2"        # General Purpose SSD (can be adjusted based on your needs)
-}
-
-# output "ebs_volume_id" {
-#   value = aws_ebs_volume.redis_volume.id
-# }
